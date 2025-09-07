fed2282db8afa23f7ff29a987d3447a0d864d144
조용원 <auau3@kakao.com>
2025-09-07 17:07:52 +0900

    포스트 개수 인디케이터 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,5 @@ import { getPosts } from "@/utils/posts";
 export default async function Page() {
   const posts = await getPosts();
 
-  return (
-    <>
-      <p className="mb-4 text-sm text-zinc-400">
-        조씨가 총 {posts.length}개 포스트를 썼습니다.
-      </p>
-      <PostList posts={posts} />
-    </>
-  );
+  return <PostList posts={posts} />;
 }
